83cd52b5d083d093aad7c80dd8bae595b14312a6
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2021-08-06 19:57:46 +0530

    Add files via upload

---
+++ cursor.js
@@ -0,0 +1,79 @@
+AFRAME.registerComponent('cursor-listener',{
+    schema:{
+        selectedItemsId:{type:'string',default:''}
+    },
+    init:function(){
+        this.handleMouseEnterEvents()
+        this.handleMouseLeaveEvents()
+    },
+    update:function(){
+        const fadeBackgroundEl = document.querySelector('#fadeBackground');
+        c = fadeBackgroundEl.children;
+        if(c.length>0){
+            var i;
+            for(i=0;i<=c.length;i++){
+                fadeBackgroundEl.removeChild(c[i]);
+            }
+        }else{
+            this.handleMouseClickEvents()
+        }
+    },
+    handlePlacesListState:function(){
+        const id = this.el.getAttribute('id')
+        const postersId = ['spiderman','justiceliague','superman','avengers']
+        if(postersId.includes(id)){
+            const posterContainer = document.querySelector('#poster-container')
+            posterContainer.setAttribute('cursor-listener',{
+                selectedItemsId:id
+            })
+            this.el.setAttribute('material',{color:'orange',opacity:1})
+        }
+    },
+    handleMouseEnterEvents:function(){
+        this.el.addEventListener('mouseenter',()=>{
+            this.handlePlacesListState()
+        })
+    },
+    handleMouseLeaveEvents:function(){
+        this.el.addEventListener('mouseleave',()=>{
+            const {selectedItemsId} = this.data
+            if(selectedItemsId){
+                const element = document.querySelector(`#${selectedItemsId}`)
+                const id = element.getAttribute('id')
+                if(id == selectedItemsId){
+                    element.setAttribute('material',{
+                        color:'white',opacity:1
+                    })
+                }
+            }
+        })
+    },
+    handleMouseClickEvents:function(){
+        this.el.addEventListener('click',e=>{
+            const {selectedItemsId} = this.data
+            const fadeBackgroundEl = document.querySelector('#fadeBackground')
+            const cursorEl = document.querySelector('#camera-cursor')
+            const titleEl = document.querySelector('#title')
+            if(selectedItemsId){
+                fadeBackgroundEl.setAttribute('visible',true)
+                fadeBackgroundEl.setAttribute('info-banner',{
+                    itemsId:selectedItemsId
+                })
+                titleEl.setAttribute('visible',false)
+                cursorEl.setAttribute('position',{x:0,y:0,z:-1})
+                cursorEl.setAttribute('geometry',{
+                    radiusInner:0.03,
+                    radiusOuter:0.04
+                })
+            }else{
+                fadeBackgroundEl.setAttribute('visible',false)
+                titleEl.setAttribute('visible',true)
+                cursorEl.setAttribute('position',{x:0,y:0,z:-3})
+                cursorEl.setAttribute('geometry',{
+                    radiusInner:0.02,
+                    radiusOuter:0.03
+                })
+            }
+        })
+    }
+})
---
+++ infoBanner.js
@@ -0,0 +1,75 @@
+AFRAME.registerComponent('info-banner',{
+    schema:{
+        itemsId:{default:'',type:'string'}
+    },
+    update:function(){
+        this.createCards()
+    },
+    createCards:function(){
+        const postersInfo = {
+            spiderman:{
+                bannerUrl:'./images/infoImage/spiderman.png',
+                title:'Spider Man',
+            },
+            justiceLeague:{
+                bannerUrl:'./images/infoImage/justiceliague.jpg',
+                title:'Justice League',
+            },
+            superman:{
+                bannerUrl:'./images/infoImage/superman.jpg',
+                title:'Super Man',
+            },
+            avengers:{
+                bannerUrl:'./images/infoImage/avengers.jpg',
+                title:'Avengers',
+            }
+        }
+        const {itemsId} = this.data
+        const fadeBackgroundEl = document.querySelector('#fadeBackground')
+        const entityEl = document.createElement('a-entity')
+        entityEl.setAttribute('visible',true)
+        entityEl.setAttribute('id',`${itemsId}-banner`)
+        entityEl.setAttribute('geometry',{
+            primitive:'plane',
+            width:1,
+            height:1
+        })
+        entityEl.setAttribute('position',{x:0,y:0,z:0})
+        entityEl.setAttribute('material',{
+            color:'black'
+        })
+        const item = postersInfo[itemsId]
+        const imageEl = this.createImageEl(item)
+        const titleEl = this.createTitleEl(item)
+        entityEl.appendChild(imageEl)
+        entityEl.appendChild(titleEl)
+    },
+    createImageEl:function(item){
+        const entityEl = document.createElement('a-entity')
+        entityEl.setAttribute('visible',true)
+        entityEl.setAttribute('geometry',{
+            primitive:'plane',
+            width:0.9,
+            height:0.8
+        })
+        entityEl.setAttribute('material',{
+            src:item.bannerUrl
+        })
+        entityEl.setAttribute('position',{y:0,z:0.6})
+        return entityEl
+    },
+    createTitleEl:function(item){
+        const entityEl = document.createElement('a-entity')
+        entityEl.setAttribute('visible',true)
+        entityEl.setAttribute('text',{
+            font:'dejavu',
+            width:2,
+            height:2,
+            color:'white',
+            anchor:'left',
+            value:item.title
+        })
+        entityEl.setAttribute('position',{y:0.2,z:0.6})
+        return entityEl
+    }
+})
